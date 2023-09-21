5b9f7dba9e0b7157fb2fa144c5058eea155cd053
arhillis <andriellerhillis@gmail.com>
2023-09-21 15:41:29 +0000

    Students added

--- app.py
+++ app.py
@@ -2,10 +2,39 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+STUDENTS = [{
+    'first_name': 'Emma',
+    'last_name': 'Woodhouse',
+    'grades': {
+        'MATH 101': 80,
+        'ENGL 101': 75,
+        'SCI 101': None,
+        'SOC_ST 101': 85
+    }
+}, {
+    'first_name': 'Elizabeth',
+    'last_name': 'Bennett',
+    'grades': {
+        'MATH 201': 88,
+        'ENGL 201': 95,
+        'SCI 201': None,
+        'SOC_ST 201': 90
+    }
+}, {
+    'first_name': 'Elinor',
+    'last_name': 'Dashwood',
+    'grades': {
+        'MATH 301': 96,
+        'ENGL 301': 88,
+        'SCI 301': None,
+        'SOC_ST 301': 87
+    }
+}]
+
 
 @app.route("/")
 def hello_world():
-  return render_template("index.html")
+  return render_template("index.html", students=STUDENTS)
 
 
 if __name__ == '__main__':
