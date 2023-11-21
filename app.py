cdbdf90cdaf26d0c1bc3349f984f5aef46aaa8e8
Andrielle Hillis <andriellerhillis@gmail.com>
2023-11-20 21:42:46 -0600

    Standard data

--- app.py
+++ app.py
@@ -9,43 +9,43 @@ CORS(app)
 
 
 
-# STUDENTS = [{
-#     'first_name': 'Emma',
-#     'last_name': 'Woodhouse',
-#     'grades': {
-#         'MATH 101': 80,
-#         'ENGL 101': 75,
-#         'SCI 101': None,
-#         'SOC_ST 101': 85
-#     }
-# }, {
-#     'first_name': 'Elizabeth',
-#     'last_name': 'Bennett',
-#     'grades': {
-#         'MATH 201': 88,
-#         'ENGL 201': 95,
-#         'SCI 201': None,
-#         'SOC_ST 201': 90
-#     }
-# }, {
-#     'first_name': 'Elinor',
-#     'last_name': 'Dashwood',
-#     'grades': {
-#         'MATH 301': 96,
-#         'ENGL 301': 88,
-#         'SCI 301': None,
-#         'SOC_ST 301': 87
-#     }
-# }]
+STUDENTS = [{
+    'first_name': 'Emma',
+    'last_name': 'Woodhouse',
+    'grades': {
+        'MATH 101': 80,
+        'ENGL 101': 75,
+        'SCI 101': None,
+        'SOC_ST 101': 85
+    }
+}, {
+    'first_name': 'Elizabeth',
+    'last_name': 'Bennett',
+    'grades': {
+        'MATH 201': 88,
+        'ENGL 201': 95,
+        'SCI 201': None,
+        'SOC_ST 201': 90
+    }
+}, {
+    'first_name': 'Elinor',
+    'last_name': 'Dashwood',
+    'grades': {
+        'MATH 301': 96,
+        'ENGL 301': 88,
+        'SCI 301': None,
+        'SOC_ST 301': 87
+    }
+}]
 
 
 @app.route("/")
 def hello_world():
-  return render_template("index.html", students=get_students())
+  return STUDENTS
 
-@app.route("/students")
-def get_students():
-    return get_students()
+# @app.route("/students")
+# def get_students():
+#     return get_students()
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=8080)
