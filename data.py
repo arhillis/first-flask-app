105f4a3b75bc58e66a34ab1b1c179211add0e9c9
Andrielle Hillis <andriellerhillis@gmail.com>
2023-11-21 07:40:19 -0600

    Getting rid of some folders

--- data.py
+++ data.py
@@ -1,11 +1,12 @@
 import os
 from xata.client import XataClient
  
-client = XataClient(api_key=os.getenv("XATA_API_KEY"), db_url="https://Andrielle-Hillis-s-workspace-ksvpaa.us-east-1.xata.sh/db/students")
+client = XataClient(api_key="xau_d8vjReqyvQgAh2JBThuyme6fbcjKV8IO5", db_url="https://Andrielle-Hillis-s-workspace-ksvpaa.us-east-1.xata.sh/db/students")
 
 def main():
     students = get_students()
-    print(students)
+    for student in students:
+        print(student)
 
 def get_students():
     data = client.data().query("students", {
@@ -16,7 +17,7 @@ def get_students():
             "grade_level"
         ]
     })
-    return data['records']
+    return [{'first_name': record['first_name'], 'last_name': record['last_name'], 'grade_level': record['grade_level']} for record in data['records']]
 
 
 if __name__ == "__main__":
