c14e9bdae14d7ca08e8fd069f7edf1eee17333ff
Andrielle Hillis <andriellerhillis@gmail.com>
2023-11-20 20:29:50 -0600

    Unignore data.py

--- data.py
+++ data.py
@@ -1,14 +1,23 @@
 import os
 from xata.client import XataClient
  
-client = XataClient(api_key=os.environ["XATA_API_KEY"], db_url="https://Andrielle-Hillis-s-workspace-ksvpaa.us-east-1.xata.sh/db/students")
+client = XataClient(api_key=os.getenv("XATA_API_KEY"), db_url="https://Andrielle-Hillis-s-workspace-ksvpaa.us-east-1.xata.sh/db/students")
 
-data = client.data().query("students", {
-    "columns": [
-        "id",
-        "first_name",
-        "last_name",
-        "grade_level"
-    ]
-})
-print(data)
+def main():
+    students = get_students()
+    print(students)
+
+def get_students():
+    data = client.data().query("students", {
+        "columns": [
+            "id",
+            "first_name",
+            "last_name",
+            "grade_level"
+        ]
+    })
+    return data['records']
+
+
+if __name__ == "__main__":
+    main()
